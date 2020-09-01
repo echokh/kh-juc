654ed6df1d2f3622ec448502044abc413a329546
echokh <252244731@qq.com>
2020-09-01 13:45:26 +0800

    add Interrupted Message

--- src/main/java/com/khstudy/juc/T01_WhatIsThread.java
+++ src/main/java/com/khstudy/juc/T01_WhatIsThread.java
@@ -4,31 +4,36 @@ import java.util.concurrent.TimeUnit;
 
 public class T01_WhatIsThread {
 
-    private static class T1 extends Thread{
+    private static class T1 extends Thread {
         @Override
         public void run() {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 1; i++) {
                 try {
                     TimeUnit.MICROSECONDS.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("t1");
+                System.out.println(this.getName());
             }
         }
     }
 
     public static void main(String[] args) {
-//        new T1().run();//这个不是线程，只是普通的run方法
-        new T1().start();
+        //   new T1().run();//这个不是线程，只是普通的run方法
+        T1 t1 = new T1();
+        t1.setName("First Thread");//给线程设置名称
+        t1.getId();//获取线程的ID
+        System.out.println("before start.."+t1.isAlive());//获取一条线程存活状态
+        t1.start();
+        System.out.println("after start.."+t1.isAlive());
         //两个方法的执行结果不同。start是交替打印，说明程序在交替执行
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             try {
                 TimeUnit.MICROSECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("main");
+//            System.out.println("main");
         }
     }
 }
---
+++ src/main/java/com/khstudy/juc/T03_1_InterruptedDemo.java
@@ -0,0 +1,46 @@
+package com.khstudy.juc;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class T03_1_InterruptedDemo {
+    volatile static AtomicInteger i = new AtomicInteger();
+    public static void main(String[] args) {
+
+        Thread t=new Thread(() -> {
+            while (true) {
+//                try {
+//                    Thread.currentThread().sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+                System.out.println(i.incrementAndGet());
+                try {
+                    System.in.read();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+
+        System.out.println(t.isInterrupted());//false
+        //将标志位设为true，但并不会打断线程的进行
+        //如果此时线程因为sleep、join、wait处于阻塞状态，则会抛出InterruptedException
+        t.interrupt();
+        System.out.println(t.isInterrupted());//true
+        Thread.interrupted();
+        System.out.println(t.isInterrupted());//false
+//        try {
+//            t.sleep(10);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(t.isInterrupted());
+
+//        while (i.intValue()==10)
+//        {
+//
+//        }
+    }
+}
