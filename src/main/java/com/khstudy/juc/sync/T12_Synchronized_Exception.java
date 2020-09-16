830b5540b21aec993f8e008ca95dd9e74af74f94
echokh <252244731@qq.com>
2020-09-16 12:28:09 +0800

    锁和异常代码完善

--- src/main/java/com/khstudy/juc/sync/T12_Synchronized_Exception.java
+++ src/main/java/com/khstudy/juc/sync/T12_Synchronized_Exception.java
@@ -4,5 +4,34 @@ package com.khstudy.juc.sync;
  * 锁和异常：synchronized在出现异常的情况下会默认释放锁，如果不想释放锁，catch异常继续运行
  */
 public class T12_Synchronized_Exception {
-    //TODO
+    public static void main(String[] args) {
+        Runnable r = new Runnable() {
+            @Override
+            public synchronized void run() {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        Thread.currentThread().sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + ":::::" + i);
+                    if (i == 5) {
+//                        double a = 5 / 0;
+                        try {
+                            double a = 5 / 0;
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+
+        Thread t1 = new Thread(r, "t1");
+        Thread t2 = new Thread(r, "t2");
+        t1.start();
+        t2.start();
+
+
+    }
 }
